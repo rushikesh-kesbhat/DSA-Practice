94fd650473b9ce26ee7e9ae9cdf3c295cc1e96dc
rushikesh-kesbhat <95136924+rushikesh-kesbhat@users.noreply.github.com>
2025-05-26 18:15:32 +0530

    Update NFactorial.java

--- 4_Recursion/Problems/NFactorial.java
+++ 4_Recursion/Problems/NFactorial.java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
 
+class NFactorial{
+	
+	public static void factorial(int n,int ans){
+		
+		if(n==1){
+			System.out.println(ans);
+			return;
+		}
+		ans*=n;
+		factorial(n-1,ans);
+	}
+	
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		
+		int n= sc.nextInt();
+		int ans=1;
+		 factorial(n,ans);
+	}
+}
