4e86769066fc3747fa29bd0ec310ce76857a3494
rushikesh-kesbhat <95136924+rushikesh-kesbhat@users.noreply.github.com>
2025-05-26 17:39:58 +0530

    Update N_NaturalNo.java

--- 4_Recursion/Problems/N_NaturalNo.java
+++ 4_Recursion/Problems/N_NaturalNo.java
@@ -1,1 +1,31 @@
+import java.util.Scanner;
 
+class N_NaturalNo{
+	
+	static void printIncreaseFromNto1(int n){
+		if(n==1){
+			System.out.println(n);
+			return;
+		}
+		System.out.println(n);
+		printIncreaseFromNto1(n-1);
+	}
+	
+	static void printIncrease(int n){
+		if(n==1){
+			System.out.println(n);
+			return;
+		}
+		printIncrease(n-1);
+		System.out.println(n);
+	}
+	
+	public static void main(String[] args){
+		Scanner sc =new Scanner(System.in);
+		System.out.println("Enter Natural No.: ");
+		int n= sc.nextInt();
+		
+		printIncreaseFromNto1(n);
+	}
+	
+}
