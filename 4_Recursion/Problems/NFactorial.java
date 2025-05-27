f4df0a28b0c97a0c54d8f80d55e764f5c996a32e
Rushikesh-kesbhat-4189-pcccs-Tybsc <rushikeshkesbhat7775@gmail.com>
2025-05-27 06:39:45 +0530

    Practice

--- 4_Recursion/Problems/NFactorial.java
+++ 4_Recursion/Problems/NFactorial.java
@@ -2,21 +2,18 @@ import java.util.Scanner;
 
 class NFactorial{
 	
-	public static void factorial(int n,int ans){
+	public static int factorial(int n){
 		
-		if(n==1){
-			System.out.println(ans);
-			return;
+		if(n==0){
+			return 1;
 		}
-		ans*=n;
-		factorial(n-1,ans);
+		return n*factorial(n-1);
 	}
 	
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
 		
 		int n= sc.nextInt();
-		int ans=1;
-		 factorial(n,ans);
+		System.out.println(factorial(n));
 	}
 }
